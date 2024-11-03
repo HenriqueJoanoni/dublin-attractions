e11bac5c213acb550f56eb4cf073678a6d31de61
HenriqueJoanoni <henrique.joanoni@gmail.com>
2024-11-03 20:17:50 +0000

    build(project): create update functionality

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -58,6 +58,9 @@ const App = () => {
     /** STATE FOR DELETE BUTTON */
     const [deleteAttraction, setDeleteAttraction] = useState([])
 
+    /** STATE FOR UPDATE BUTTON */
+    const [updatedAttraction, setUpdatedAttraction] = useState([])
+
     /** STATE FOR PAGINATION */
     const [postsPerPage] = useState(10)
     const [currentPage, setCurrentPage] = useState(1)
@@ -166,8 +169,27 @@ const App = () => {
             free: newAttractionData.attrFree
         }
 
-        setLocations((prevLocations) => [...prevLocations, newAttraction])
-        setDeleteAttraction((allLocations) => [...allLocations, newAttraction])
+        setLocations((prevLocations) => {
+            const index = prevLocations.findIndex((item) => item.id === newAttraction.id)
+            if (index !== -1) {
+                const updatedLocations = [...prevLocations]
+                updatedLocations[index] = newAttraction
+                return updatedLocations
+            } else {
+                return [...prevLocations, newAttraction]
+            }
+        })
+
+        setDeleteAttraction((allLocations) => {
+            const index = allLocations.findIndex((item) => item.id === newAttraction.id)
+            if (index !== -1) {
+                const updatedDeleteAttraction = [...allLocations]
+                updatedDeleteAttraction[index] = newAttraction
+                return updatedDeleteAttraction
+            } else {
+                return [...allLocations, newAttraction]
+            }
+        })
     }
 
     /** HANDLE DELETE ATTRACTION */
@@ -268,10 +290,6 @@ const App = () => {
                         <button className={"insert-button"}
                                 onClick={() => handleShowModalForms("insert new attraction")}>Insert new
                         </button>
-                        {/* UPDATE BUTTON */}
-                        <button className={"update-button crud-buttons"}
-                                onClick={() => handleShowModalForms("update attraction")}>Update Attraction
-                        </button>
                     </div>
                 </div>
             </div>
@@ -330,20 +348,39 @@ const App = () => {
                                             ) : ("No Tags")}
                                         </td>
                                         <td data-title="rating">{item.rating}</td>
-                                        <td data-title="description">{searchFreeAttractions(item.description) ? 'Free' : "No"}</td>
-                                        <td data-title="delete-item">
-                                            <button
-                                                className="delete-button crud-buttons"
-                                                onClick={() => handleDeleteElement(item.id)}
-                                            >
-                                                <img
-                                                    src="/src/assets/img/recycle-bin.png"
-                                                    width="20"
-                                                    height="20"
-                                                    alt="trash icon"
-                                                    title="Delete Element"
-                                                />
-                                            </button>
+                                        <td data-title="free-attraction">
+                                            {(searchFreeAttractions(item.description) || item.free) ? 'Free' : 'No'}
+                                        </td>
+                                        <td data-title="action-buttons">
+                                            <div className="button-group">
+                                                <button
+                                                    className="update-button crud-buttons"
+                                                    onClick={() => {
+                                                        handleShowModalForms("update attraction")
+                                                        setUpdatedAttraction(item)
+                                                    }}
+                                                >
+                                                    <img
+                                                        src="/src/assets/img/pencil.png"
+                                                        width="20"
+                                                        height="20"
+                                                        alt="pencil icon"
+                                                        title="Edit Element"
+                                                    />
+                                                </button>
+                                                <button
+                                                    className="delete-button crud-buttons"
+                                                    onClick={() => handleDeleteElement(item.id)}
+                                                >
+                                                    <img
+                                                        src="/src/assets/img/recycle-bin.png"
+                                                        width="20"
+                                                        height="20"
+                                                        alt="trash icon"
+                                                        title="Delete Element"
+                                                    />
+                                                </button>
+                                            </div>
                                         </td>
                                     </tr>
                                 ))}
@@ -397,7 +434,7 @@ const App = () => {
                     <Modal.Body>
                         {currentForm.toLowerCase().includes('insert') ?
                             <InsertForm onSubmit={handleFormSubmit}/> :
-                            <UpdateForm/>
+                            <UpdateForm updatedAttraction={updatedAttraction} onSubmit={handleFormSubmit}/>
                         }
                     </Modal.Body>
                 </Modal>
@@ -427,7 +464,7 @@ const InsertForm = ({onSubmit}) => {
 
     /* HANDLE FORM CHANGE */
     const handleChange = (ev) => {
-        const { name, value, type, checked } = ev.target
+        const {name, value, type, checked} = ev.target
         setFormData((prevData) => ({
             ...prevData,
             [name]: type === "checkbox" ? checked : value
@@ -540,8 +577,8 @@ const InsertForm = ({onSubmit}) => {
             {/**********************************************************************************************************
              *                                  BELOW CODE CODE BASED ON:                                               *
              * - https://cloudinary.com/blog/how-to-upload-multiple-images-at-once-in-react                             *
-            *                                                                                                          *
-            **********************************************************************************************************/}
+             *                                                                                                          *
+             **********************************************************************************************************/}
 
             <div className="row mt-2">
                 <div className="col">
@@ -575,7 +612,8 @@ const InsertForm = ({onSubmit}) => {
                             <label key={num} className="form-check form-check-inline">
                                 <input
                                     className="form-check-input"
-                                    type="checkbox"
+                                    type="radio"
+                                    name="attrRating"
                                     checked={formData.attrRating === num}
                                     onChange={() => handleRatingChange(num)}
                                 />
@@ -607,13 +645,212 @@ const InsertForm = ({onSubmit}) => {
 }
 
 /* UPDATE FORM */
-const UpdateForm = () => {
+const UpdateForm = ({updatedAttraction, onSubmit}) => {
+    const [formData, setFormData] = useState({
+        attrId: "",
+        attrName: "",
+        attrLatitude: "",
+        attrLongitude: "",
+        attrAddress: "",
+        attrDescription: "",
+        attrPhoneNumber: "",
+        attrPhotoURL: [],
+        attrTags: "",
+        attrRating: 0,
+        attrFree: false
+    })
+
+    useEffect(() => {
+        if (updatedAttraction) {
+            setFormData({
+                attrId: updatedAttraction.id,
+                attrName: updatedAttraction.name,
+                attrLatitude: updatedAttraction.latitude,
+                attrLongitude: updatedAttraction.longitude,
+                attrAddress: updatedAttraction.address,
+                attrDescription: updatedAttraction.description,
+                attrPhoneNumber: updatedAttraction.phoneNumber || "",
+                attrPhotoURL: updatedAttraction.photosURLs || [],
+                attrTags: updatedAttraction.tags ? updatedAttraction.tags.join(", ") : "",
+                attrRating: updatedAttraction.rating,
+                attrFree: updatedAttraction.free
+            })
+        }
+    }, [updatedAttraction])
+
+    const handleChange = (ev) => {
+        const {name, value, type, checked} = ev.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
+
+    const handleRatingChange = (rating) => {
+        setFormData((prevData) => ({
+            ...prevData,
+            attrRating: rating
+        }))
+    }
+
+    const handleUploadImage = (ev) => {
+        const filesArray = Array.from(ev.target.files)
+        setFormData((prevData) => ({
+            ...prevData,
+            attrPhotoURL: filesArray
+        }))
+    }
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        onSubmit(formData)
+    }
+
     return (
-        <form>
-            <label htmlFor="fname">First name:</label><br/>
-            <input type="text" id="fname" name="fname"/><br/>
-            <label htmlFor="lname">Last name:</label><br/>
-            <input type="text" id="lname" name="lname"/>
+        <form onSubmit={handleSubmit}>
+            <div className="row">
+                <div className="col">
+                    <label htmlFor="attrId">Attraction ID</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="attrId"
+                        value={formData.attrId}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className="col">
+                    <label htmlFor="attrName">Attraction Name</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="attrName"
+                        value={formData.attrName}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <div className="row mt-2">
+                <div className="col">
+                    <label htmlFor="attrLatitude">Latitude</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="attrLatitude"
+                        value={formData.attrLatitude}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className="col">
+                    <label htmlFor="attrLongitude">Longitude</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="attrLongitude"
+                        value={formData.attrLongitude}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <div className="mt-2">
+                <label htmlFor="attrAddress">Address</label>
+                <input
+                    className="form-field w-100"
+                    type="text"
+                    name="attrAddress"
+                    value={formData.attrAddress}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="mt-2">
+                <label htmlFor="attrDescription">Description</label>
+                <textarea
+                    className="form-control"
+                    name="attrDescription"
+                    cols="30"
+                    rows="5"
+                    value={formData.attrDescription}
+                    onChange={handleChange}>
+            </textarea>
+            </div>
+
+            <div className="mt-2">
+                <label htmlFor="attrPhoneNumber">Phone Number</label>
+                <input
+                    className="form-field w-100"
+                    type="text"
+                    name="attrPhoneNumber"
+                    value={formData.attrPhoneNumber}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="row mt-2">
+                <div className="col">
+                    <label htmlFor="attrPhotoURL">Photos</label>
+                    <input
+                        className="form-control"
+                        type="file"
+                        name="attrPhotoURL"
+                        multiple
+                        onChange={handleUploadImage}
+                    />
+                </div>
+
+                <div className="col">
+                    <label htmlFor="attrTags">Tags</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="attrTags"
+                        value={formData.attrTags}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <div className="mt-2">
+                <div className="form-group">
+                    <label>Rating</label>
+                    <div>
+                        {[1, 2, 3, 4, 5].map((num) => (
+                            <label key={num} className="form-check form-check-inline">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="attrRating"
+                                    value={num}
+                                    checked={formData.attrRating === num}
+                                    onChange={() => handleRatingChange(num)}
+                                />
+                                {num}
+                            </label>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="form-check-label" htmlFor="attrFree">
+                        Free attraction?
+                    </label>
+                    <input
+                        className="form-check-input ms-2"
+                        type="checkbox"
+                        name="attrFree"
+                        checked={formData.attrFree}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <Modal.Footer>
+                <Button variant="primary" type="submit">Update</Button>
+            </Modal.Footer>
         </form>
     )
 }
